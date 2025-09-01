1785a9014c8480879a82f43d6ce10bc31ee7ff2d
TheSarthak08 <sarthak.kaushik08.07.2004@gmail.com>
2025-09-01 23:37:23 +0530

    Create AdditionalQuestion3.cpp

---
+++ assignment2/AdditionalQuestion3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string checkAnagram(string str1, string str2) {
+    if (str1.length() != str2.length()) {
+        return "NO";
+    }
+    
+    // Sort both strings
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    
+    // Compare sorted strings
+    if (str1 == str2) {
+        return "YES";
+    }
+    return "NO";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        string str1, str2;
+        cin >> str1 >> str2;
+        cout << checkAnagram(str1, str2) << endl;
+    }
+    
+    return 0;
+}
