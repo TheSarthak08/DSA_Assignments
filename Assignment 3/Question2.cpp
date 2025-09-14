6f0aa18d41699ed47a361d914b47ddbe0103bd1b
TheSarthak08 <sarthak.kaushik08.07.2004@gmail.com>
2025-09-14 12:52:33 +0530

    Update Question2.cpp

--- Assignment 3/Question2.cpp
+++ Assignment 3/Question2.cpp
@@ -1,28 +1,23 @@
 /*Given a string, reverse it using STACK. For example “DataStructure” should be output as
 “erutcurtSataD.”*/
 
-#include<iostream>
+#include <iostream>
 #include<stack>
 using namespace std;
-
-void ReverseString(string &s){
-    stack<char>st;
-    int n=s.length();
-    for(int i=0; i<n; i++){
+void revstring(string &s){
+    stack <char> st;
+    int n = s.length();
+    for(int i = 0;i<n;i++){
         st.push(s[i]);
     }
-
-    for(int i=0; i<n; i++){
+    for(int i = 0;i<n;i++){
         s[i]=st.top();
         st.pop();
     }
-    
 }
-
 int main(){
-    string sg="DataStructure";
-    ReverseString(sg);
-    cout<<sg;
-
+    string s = "Data Structure";
+    revstring(s);
+    cout<<s;
     return 0;
 }
